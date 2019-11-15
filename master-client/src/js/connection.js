e3450a7e248f0b0a650ebcd4fbd55e0c17c64dc9
Jack Hayford <jack.hayford@intersection.com>
2019-11-15 12:18:07 -0500

    Update server location

--- master-client/src/js/connection.js
+++ master-client/src/js/connection.js
@@ -3,7 +3,8 @@ import * as Actions from './store/actions';
 import { getStatus } from './store/selectors';
 
 // const API_BASE = 'localhost:3000';
-const API_BASE = 'http://api.arkade.ngrok.io';
+// const API_BASE = 'http://api.arkade.ngrok.io';
+const API_BASE = '35.243.255.242';
 
 class Connection {
   init(store) {
--- remote-client/src/js/connection.js
+++ remote-client/src/js/connection.js
@@ -2,7 +2,8 @@ import io from 'socket.io-client';
 import * as Actions from './store/actions';
 
 // const API_BASE = 'localhost:3000';
-const API_BASE = 'http://api.arkade.ngrok.io';
+// const API_BASE = 'http://api.arkade.ngrok.io';
+const API_BASE = '35.243.255.242';
 
 class Connection {
   init(store) {
